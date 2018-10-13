5b68af27d5a44413dccedb2824b9a40653556c5e
Rahul-m-bhat <44108924+Rahul-m-bhat@users.noreply.github.com>
2018-10-13 12:00:54 +0530

    Add files via upload

---
+++ is_palindrome.py
@@ -0,0 +1,15 @@
+def palindrome(string):
+	s = string.lower()
+	s = s.replace(" ","")
+	h = s[::-1]
+	if s == h:
+		print("the '%s' is a palindrome" % (string))
+	else:
+		print("the '%s' is not a palindrome" % (string))
+
+
+#running the program with input as "nurses Run"
+palindrome("nurses Run")
+
+#running the program with input as "Today is a good day"
+palindrome("Today is a good day")
